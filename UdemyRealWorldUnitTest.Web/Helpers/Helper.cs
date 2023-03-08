726a3f048d8fafe4b33e740c73c5d6593a2c5212
denizdemirdoven@gmail.com <Deniz Demirdoven>
2023-03-08 22:40:20 +0300

    Add project files.

---
+++ UdemyRealWorldUnitTest.Web/Helpers/Helper.cs
@@ -0,0 +1,14 @@
+﻿namespace UdemyRealWorldUnitTest.Web.Helpers
+{
+    public class Helper
+    {
+
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+
+
+    }
+}
